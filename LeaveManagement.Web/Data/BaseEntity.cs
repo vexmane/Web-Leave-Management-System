b4e34ccdbf0c17a54fac6d291f610109449614d8
winvex <winvex@DESKTOP-5VUAN58>
2024-03-17 12:04:56 +0200

    first

---
+++ LeaveManagement.Web/Data/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagement.Web.Data
+{
+    public partial class BaseEntity // Using partial so BaseEntity cannot be instantiated by itself
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
